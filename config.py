0b5fd1b73ed2e4c8533ee86530609d9945f0188f
moha1091 <med.zouggagh@gmail.com>
2024-09-01 23:56:18 +0100

    Adding Files

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', os.urandom(24))
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL','postgres://default:1DWbvKocPrp0@ep-plain-union-a2rcbea6-pooler.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require')
-    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URL',
+        'postgresql://default:1DWbvKocPrp0@ep-plain-union-a2rcbea6-pooler.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
